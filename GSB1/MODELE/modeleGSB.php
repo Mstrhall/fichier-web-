7732d3355384cf5ed58f05d3d45a40e359272f32
Theo GARDAIS <TGARDAIS@SIO.LOCAL>
2023-03-21 11:38:29 +0100

    affichage des infos des médicaments sur la page medicament

--- GSB1/MODELE/modeleGSB.php
+++ GSB1/MODELE/modeleGSB.php
@@ -1,1 +1,35 @@
 <?php
+
+function init(){
+    $url = 'https://localhost:44306/WebService1.asmx?WSDL';
+
+    $option = array(
+        'cache_wsdl' => 0,
+        'trace' => 1,
+        'stream_context' => stream_context_create(array(
+            'ssl' => array(
+                'verify_peer'=>false,
+                'verify_peer_name'=>false,
+                'allow_self_signed'=>true
+            )
+        ))
+    );
+
+    $client = new SoapClient($url, $option);
+
+    return $client;
+}
+function getMedicaments(){
+    $client=init();
+
+    $res=$client->select_medicaments();
+    $lesRes=$res->select_medicamentsResult->string;
+    $tab=array();
+
+    for($i=0;$i<3;$i++){
+        $tab[$i]=explode(";",$lesRes[$i]);
+    }
+    var_dump($tab);
+}
+
+?>
--- GSB1/VUE/Medicament.php
+++ GSB1/VUE/Medicament.php
@@ -14,7 +14,7 @@
     <div class="row">
         <div class=" col-3">
             <a href="index.php">
-                <img src="VUE/logo.png" class="img-responsive d-inline-block align-top" alt="LOGO GSB" width="300">
+                <img src="VUE/lelogo.png" class="img-responsive d-inline-block align-top" alt="LOGO GSB" width="300">
             </a>
         </div>
 
@@ -27,3 +27,12 @@
     </div>
 </div>
 <h1>MEDICAMENT</h1>
+<p>Chez GSB nous proposont une large game de médicaments pour les professionels de santé.
+    Sur cette page vous trouverez toutes les information nessessaires sur vos médicaments. A l'aide des informations que nous vous fournissont
+plus bas vous serez en capacité de voir quel médicament est contre-indiqué mais aussi les effets thérapeutiques et secondaires de chacun.</p>
+<div>
+    <?php require_once "MODELE/modeleGSB.php";
+
+    getMedicaments();
+    ?>
+</div>
