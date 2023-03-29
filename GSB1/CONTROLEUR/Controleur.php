5ead8f07e2e85df940a7ef88251905e827e45a93
celian LAUMOND <98083196+Mstrhall@users.noreply.github.com>
2023-03-29 17:48:39 +0200

    Add files via upload

--- GSB1/CONTROLEUR/Controleur.php
+++ GSB1/CONTROLEUR/Controleur.php
@@ -6,9 +6,39 @@ function appelMENTION(){
     require_once "VUE/MentionsLegales.php";
 }
 function appelMEDOC(){
+    $medicaments = getMedicaments();
     require_once "VUE/Medicament.php";
 }
 
 function appelACTIVITE(){
+    $activites=getActivite();
     require_once "VUE/activité.php";
+
+}
+
+function appelDetailActivite(){
+    $activite=getVerifParticipant()
+}
+
+function verifparticipant($sontadresse){
+    $verifparticipant=getVerifParticipant($sontadresse);
+
+    return $verifparticipant;
+}
+function  getEffetsecondaire($iddumedoc){
+    $medicament=geteffesecondaire($iddumedoc);
+    return$medicament;
+}
+function getEfftherapeutique($iddumedoc){
+    $medicament=geteffetherapeutique($iddumedoc);
+    return $medicament;
+}
+
+function insertparticipants($nom,$prenom,$adresse,$num){
+    insertparticipant($nom,$prenom,$adresse,$num);
+}
+
+function insertParticiper($idActviter,$idParticiper){
+    insertParticiper($idActviter,$idParticiper);
+
 }
