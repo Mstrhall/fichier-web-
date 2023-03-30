24b596de26d8927adce7935d1745befa857885dc
celian LAUMOND <98083196+Mstrhall@users.noreply.github.com>
2023-03-30 09:29:28 +0200

    Add files via upload

---
+++ GSB1/VUE/reserver.php
@@ -0,0 +1,49 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>ACCEUIL</title>
+    <link rel="stylesheet" type="text/css" href="VUE/style.css">
+    <link rel="stylesheet" type="text/css" href="VUE/css/bootstrap.css">
+</head>
+<body>
+
+<div class="container">
+    <!--<iframe src="VUE/menu.php" width="100%" height="60px" frameborder="0"></iframe>-->
+    <div class="row">
+        <div class=" col-3">
+            <a href="index.php">
+                <img src="VUE/logo.png" class="img-responsive d-inline-block align-top" alt="LOGO GSB" width="300">
+            </a>
+        </div>
+
+
+        <div class="col-9">
+
+            <?php require_once "VUE/menu.php";?>
+
+        </div>
+    </div>
+</div>
+<h1>Reste t'il des places </h1>
+<table>
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Nom</th>
+
+    </tr>
+    </thead>
+
+    <tbody>
+    <?php foreach ($activites as $activite): ?>
+        <tr>
+            <td><?php echo $activite[1]; ?></td>
+            <td><?php echo $activite[2]; ?></td>
+            <td><input type="link" name="complet" value= <a href="index.php?action=pageComplet">complet/></td>
+        </tr>
+    <?php endforeach;?>
+    <!-- Ajoutez autant de lignes que nécessaire -->
+    </tbody>
+</table>
