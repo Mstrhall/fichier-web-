395e0d247f5ec0f8c14ca83b5a7839db28e17218
celian LAUMOND <98083196+Mstrhall@users.noreply.github.com>
2023-03-31 12:05:41 +0200

    Update effets.php

--- GSB1/VUE/effets.php
+++ GSB1/VUE/effets.php
@@ -1,19 +1,42 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
+
+        <meta charset="UTF-8">
     <title>Informations sur un médicament</title>
-  </head>
-  <body>
-    <h1>Informations sur un médicament</h1>
-    <h2>Nom médicament : </h2>
-    <?php echo $nomMedoc ?>
-    <h2>Effet thérapeutique(s) :</h2>
-    <?php foreach ($effetsTerap as $unEffetTerap){
-      echo $unEffetTerap;
-    }?>
-    <h2>Effet secondaire(s)</h2>
-    <?php foreach ($effetsSecond as $unEffetSecond){
-      echo $unEffetTerap;
-    }?>
-  </body>
+        <link rel="stylesheet" type="text/css" href="VUE/style.css">
+        <link rel="stylesheet" type="text/css" href="VUE/css/bootstrap.css">
+        <div class="container">
+            <!--<iframe src="VUE/menu.php" width="100%" height="60px" frameborder="0"></iframe>-->
+            <div class="row">
+                <div class=" col-3">
+                    <a href="index.php">
+                        <img src="VUE/lelogo.png" class="img-responsive d-inline-block align-top" alt="LOGO GSB" width="300">
+                    </a>
+                </div>
+
+
+                <div class="col-9">
+
+                    <?php require_once "VUE/menu.php";?>
+
+                </div>
+    </head>
+
+</head>
+<body>
+<h1>Informations sur un médicament</h1>
+<h2>Nom médicament : </h2>
+<?php echo $nomMedoc ?>
+<h2>Effet thérapeutique(s) :</h2>
+<?php foreach ($effetsTerap as $unEffetTerap){
+   echo $unEffetTerap[1];
+   echo "<br>";
+}?>
+<h2>Effet secondaire(s)</h2>
+<?php foreach ($effetsSecond as $unEffetSecond){
+    echo $unEffetSecond[1];
+	echo "<br>";
+}?>
+</body>
 </html>
